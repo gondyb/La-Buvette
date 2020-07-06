48a69e916661c8437f506de7abfd101ac59d9d31
Benjamin Gondange <benjamin@gondange.com>
2020-07-06 12:20:29 +0200

    Initial commit

---
+++ src/main/java/fr/sasvb/labuvette/LaBuvetteApplication.java
@@ -0,0 +1,13 @@
+package fr.sasvb.labuvette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBuvetteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaBuvetteApplication.class, args);
+    }
+
+}
